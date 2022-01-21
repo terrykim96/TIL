af49c804d48fa2bfe00b505cc042f7829afe787d
terrykim96 <terrykim96@naver.com>
2022-01-21 20:59:46 +0900

    Solve 2 swea problem(백만장자, 369게임)

--- Algorithm/swea_1859_백만장자.py
+++ Algorithm/swea_1859_백만장자.py
@@ -3,20 +3,12 @@ T = int(input())
 for i in range(T):
     days = int(input())
     price = list(map(int, input().split()))
-    buy = 0
-    product = 0
     profit = 0
+    max_price = price[-1]           # 마지막날 금액을 최대 금액으로 설정
+    for j in range(days - 2, -1, -1):   # 마지막날 전날부터 거꾸로 탐색하기
+        if price[j] < max_price:        # 가격이 최대 금액보다 작으면 이익에 두 가격의 차액 더하기
+            profit += (max_price - price[j])
+        else:                       # 가격이 최대금액보다 크면 최대 금액 바꿔주기
+            max_price = price[j]
 
-    for daily_price in price:
-        if daily_price < max(price):
-            buy += daily_price
-            product += 1
-            price = price[1:]
-        else:
-            profit += (daily_price * product - buy)
-            product =0
-            buy = 0
-            price = price[1:]
-        print(buy, product)
-    profit += (price[0] * product - buy)
     print(f'#{i + 1} {profit}')
---
+++ Algorithm/swea_1926_간단한369.py
@@ -0,0 +1,34 @@
+N = int(input())
+
+for i in range(N):
+    if '3' in str(i+1):         # 숫자에 3이 들어가면
+        cnt = 0            
+        for j in str(i+1):  # 3, 6, 9가 들어가는 갯수만큼 cnt를 증가시킴
+            if j in ['3', '6', '9']:
+                cnt += 1
+        print('-'*cnt, end= ' ')    # cnt 갯수만큼 '-' 출력
+    elif '6' in str(i+1):   # 6과 9에 대해서도 같은 방식으로 진행
+        cnt = 0
+        for j in str(i+1):
+            if j in ['3', '6', '9']:
+                cnt += 1
+        print('-'*cnt, end= ' ')
+    elif '9' in str(i+1):
+        cnt = 0
+        for j in str(i+1):
+            if j in ['3', '6', '9']:
+                cnt += 1
+        print('-'*cnt, end= ' ')       
+    else:
+        print(i+1, end=' ')     # 3 6 9 아무것도 없다면 숫자 출력
+# ----------------------------------------------------
+
+# check_num 세개에 각각 3, 6, 9 저장
+check_num1, check_num2, check_num3 = '3', '6', '9'
+
+# 숫자의 문자열에서 check_num을 세어 총 숫자를 clap_num에 저장
+for num in range(1, N+1):
+    clap_num = str(num).count(check_num1) + str(num).count(check_num2) + str(num).count(check_num3)
+    if not clap_num:        # clap_num이 0이면 숫자 출력
+        print(num, end= '')
+    print('-'*clap_num, end= ' ')   # 0이 아니면 숫자만큼 '-' 출력
