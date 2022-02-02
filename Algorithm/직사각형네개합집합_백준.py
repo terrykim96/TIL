102d7f3487500bd74ac447935104159f7b8bd48c
terrykim96 <terrykim96@naver.com>
2022-02-02 22:54:51 +0900

    Solve 직사각형 네개.py

--- Algorithm/직사각형네개합집합_백준.py
+++ Algorithm/직사각형네개합집합_백준.py
@@ -3,12 +3,12 @@ area = [[0]*100 for _ in range(100)]    # 전체 좌표평면을 만든다
 for _ in range(4):
     x1, y1, x2, y2 = map(int, input().split())  # 각 좌표를 저장
 
-
     for i in range(x1, x2):                 # 저장된 좌표에 대해
         for j in range(y1, y2):
             area[i][j] += 1                 # 좌표평면에서 해당하는 좌표 범위 내의 숫자를 표시 (+1로)
 
 width = 0
+
 for i in range(len(area)):              # 표시가 끝나면 칠해진 전체 범위를 탐색하며 0이 아니면 넓이를 추가
     for j in range(len(area)):
         if area[i][j] > 0:
