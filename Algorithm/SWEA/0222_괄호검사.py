39d70ef08be3d9a930300e00c6af88fd2e0140be
terrykim96 <terrykim96@naver.com>
2022-02-22 15:47:07 +0900

    Solve 0222 SSAFY homework

--- Algorithm/SWEA/0222_괄호검사.py
+++ Algorithm/SWEA/0222_괄호검사.py
@@ -6,9 +6,10 @@ def check(lst):
         if i in ('{', '('):
             stack.append(i)
         elif i in ('}', ')'):
-            if stack[-1] != target[i] or len(stack) == 0:
+            if len(stack) == 0 or stack[-1] != target[i]:
                 return 0
-            stack.pop()
+            else:
+                stack.pop()
     if stack:
         return 0
     else:
@@ -16,5 +17,6 @@ def check(lst):
     
 T = int(input())
 
-for test_case in range(1, T + 1):
-    print(f'#{test_case} {check(input())}')
+for tc in range(1, T + 1):
+    string = input()
+    print(f'#{tc} {check(string)}')
