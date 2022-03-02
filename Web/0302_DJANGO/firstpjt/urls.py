d252e7924e3342be8bd750a01b2b43125459cc86
terrykim96 <terrykim96@naver.com>
2022-03-02 14:33:20 +0900

    0302 SSAFY DJANGO

---
+++ Web/0302_DJANGO/firstpjt/urls.py
@@ -0,0 +1,30 @@
+"""firstpjt URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path
+
+from articles import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+
+    # http://127.0.0.1:8000/hello/라는 요청이 들어왔을때,
+    # hello라는 함수를 호출한다.
+    # path('hello/', hello)
+
+    path('articles/', views.index),
+]
