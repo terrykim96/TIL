1fc8abe399b2a2b9889b3453a4b3b7ff00c375db
terrykim96 <terrykim96@naver.com>
2022-03-24 12:37:05 +0900

    0324 SSAFY 오전

--- Algorithm/백준/퇴사_백준_14501.py
+++ Algorithm/백준/퇴사_백준_14501.py
@@ -5,12 +5,13 @@ table = []
 for i in range(N):
     table.append(list(map(int, input().split())))
 
-ans = [0 for i in range(N + 1)]
+ans = [0] * (N + 1)
 
 for i in range(N - 1, -1, -1):      # 백만장자처럼 뒤에서부터 시작해서 N번째날의 금액을 정해준다.
-    if (i + table[i][0]) > N:       # (N번째 날 기준 수익 + Tn만큼 지난 후 수익)과 (N + 1번째날 수익) 중 더 큰게 N번째 날 수익
+    if (i + table[i][0]) > N:       
         ans[i] = ans[i + 1]         
-    else:                           
+    else:                           # (N번째 날 기준 수익 + Tn만큼 지난 후 수익)과 (N + 1번째날 수익) 중 더 큰게 N번째 날 수익
         ans[i] = max(table[i][1] + ans[i + table[i][0]], ans[i + 1])
+    # print(ans)
 
 print(ans[0])
